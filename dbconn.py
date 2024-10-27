648ce592f29f81fd9d93b0767414c845cdb1d87a
Osei Bonsu <73352505+bonsuot@users.noreply.github.com>
2024-10-27 13:49:55 -0400

    updated database connection
    pointing connection to oracle cloud

--- dbconn.py
+++ dbconn.py
@@ -1,35 +1,72 @@
 import oracledb
+import os
+from datetime import datetime
+from dotenv import load_dotenv
 
-cs = 'localhost/XEPDB1'
+# Load environment variables
+load_dotenv()
 
-# connect to Oracle db
-def connect_to_db():
-        """
-        create Oracle databse connection
-        Returns:
-            tuple: connection, cursor
-        """
+# Access variables
+username = os.getenv("ORACLE_USERNAME")
+password = os.getenv("ORACLE_PASSWORD")
+cs = os.getenv("ORACLE_DSN")
+dir = os.getenv("lib_dir")
+
+
+"""
+Pointing databse connection to Oracle Cloud
+For local connection, use commented code below this
+"""
+# connect to Oracle Cloud db
+def connect_to_cloud_db():
         try:
 
             try:
-                oracledb.init_oracle_client()
+                oracledb.init_oracle_client(lib_dir=dir)
             except Exception:
                     # If already initialized or thick mode not available, continue
                 pass
             
-            connection = oracledb.connect(user="your_username", password="password", dsn=cs)
-            # Create a cursor
+            if not all([username, password]):
+                    raise ValueError("Database credentials are not fully set in the environment variables.")    
+            connection = oracledb.connect(user=username, password=password,
+                                          dsn=cs)
+                # Create a cursor
             cursor = connection.cursor()
-            print("\n")
-            print("=" * 50)
-            print("Connection to Oracle database successful!")
-            print("=" * 50)
+            print(f"Successfully connected to Oracle Database at {datetime.now()}")
             return connection, cursor
 
-   
         except oracledb.Error as e:
             error_obj, = e.args
             print(f"Error Code: {error_obj.code}")
             print(f"Error Message: {error_obj.message}")
             return None, None
+
+
+# cs = 'localhost/XEPDB1' #connection string
+
+# def connect_to_db():
+#         """
+#         create Oracle databse connection
+#         Returns:
+#             tuple: connection, cursor
+#         """
+#         try:
+
+#             try:
+#                 oracledb.init_oracle_client()
+#             except Exception:
+#                     # If already initialized or thick mode not available, continue
+#                 pass
+            
+#             connection = oracledb.connect(user="your username", password="password", dsn=cs)
+#             # Create a cursor
+#             cursor = connection.cursor()
+#             return connection, cursor
+   
+#         except oracledb.Error as e:
+#             error_obj, = e.args
+#             print(f"Error Code: {error_obj.code}")
+#             print(f"Error Message: {error_obj.message}")
+#             return None, None
 
